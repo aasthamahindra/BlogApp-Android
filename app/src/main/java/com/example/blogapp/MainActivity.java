f9dfb1415cce3ae32d845becc5f134e7f8d437d2
Aastha Mahindra <69888214+aastha-m125@users.noreply.github.com>
2021-08-18 20:22:28 +0530

    Blog list page

--- app/src/main/java/com/example/blogapp/MainActivity.java
+++ app/src/main/java/com/example/blogapp/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.blogapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -47,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
 
                 if(mUser != null){
                     Toast.makeText(MainActivity.this, "Signed in", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, "Not signed in", Toast.LENGTH_LONG).show();
                 }
             }
         };
@@ -76,16 +81,41 @@ public class MainActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()){
                     Toast.makeText(MainActivity.this, "Signed in", Toast.LENGTH_LONG).show();
+
+                    startActivity(new Intent(MainActivity.this, PostListActivity.class));
                 }else{
-                    Toast.makeText(MainActivity.this, "Not Signed in", Toast.LENGTH_LONG).show();
+                    //something
+
                 }
             }
         });
     }
 
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() ==  R.id.action_signout){
+            mAuth.signOut();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuth != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
