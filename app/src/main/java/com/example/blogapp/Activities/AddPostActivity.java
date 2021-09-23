2c60b0ffdab69465d4380b0c183da5c66d0819fd
Aastha Mahindra <69888214+aastha-m125@users.noreply.github.com>
2021-09-23 15:12:28 +0530

    Saving a post

--- app/src/main/java/com/example/blogapp/Activities/AddPostActivity.java
+++ app/src/main/java/com/example/blogapp/Activities/AddPostActivity.java
@@ -2,7 +2,11 @@ package com.example.blogapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.LauncherActivity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.LauncherActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -14,21 +18,30 @@ import android.widget.Toast;
 import com.example.blogapp.Model.Blog;
 import com.example.blogapp.R;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddPostActivity extends AppCompatActivity {
-
     private ImageButton mPostImage;
     private EditText mPostTitle;
     private EditText mPostDesc;
     private Button mSubmitButton;
+    private StorageReference mStorage;
     private DatabaseReference mPostDatabase;
-    private FirebaseUser mUser;
     private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
     private ProgressDialog mProgress;
+    private Uri mImageUri;
+    private static final int GALLERY_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,43 +52,83 @@ public class AddPostActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
-        mPostDatabase = FirebaseDatabase.getInstance().getReference().child("mBlog");
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        mPostDatabase = FirebaseDatabase.getInstance().getReference().child("MBlog");
 
         mPostImage = (ImageButton) findViewById(R.id.imageButton);
         mPostTitle = (EditText) findViewById(R.id.postTitleEt);
         mPostDesc = (EditText) findViewById(R.id.postTextEt);
         mSubmitButton = (Button) findViewById(R.id.submitButtonEt);
 
-        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+        mPostImage.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                //Post to database
-                startPosting();
-            }
-
-            private void startPosting() {
-                mProgress.setMessage("Posting to Blog...");
-                mProgress.show();
-
-                String titleVal = mPostTitle.getText().toString().trim();
-                String descVal = mPostDesc.getText().toString().trim();
-
-                if(!TextUtils.isEmpty(titleVal) && !TextUtils.isEmpty(descVal)){
-                    //Start uploading to database
-                    Blog blog = new Blog("title", "description", "imageurl",
-                            "timestamp", "userid");
-                    mPostDatabase.setValue(blog).addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void unused) {
-                            Toast.makeText(getApplicationContext(), "Item added",
-                                    Toast.LENGTH_LONG).show();
-                            mProgress.dismiss();
-                        }
-                    });
-
-                }
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivity(galleryIntent);
             }
         });
 
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Posting to our database
+                startPosting();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_CODE && resultCode == RESULT_OK) {
+            mImageUri = data.getData();
+            mPostImage.setImageURI(mImageUri);
+        }
+    }
+
+    private void startPosting() {
+
+        mProgress.setMessage("Posting to blog...");
+        mProgress.show();
+
+        final String titleVal = mPostTitle.getText().toString().trim();
+        final String descVal = mPostDesc.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(titleVal) && !TextUtils.isEmpty(descVal)
+                && mImageUri != null) {
+            //start the uploading...
+            //mImageUri.getLastPathSegment() == /image/myphoto.jpeg"
+
+            final StorageReference filepath = mStorage.child("MBlog_images").
+                    child(mImageUri.getLastPathSegment());
+            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    Task<Uri> downloadUrl = taskSnapshot.getMetadata().getReference().getDownloadUrl();
+                    DatabaseReference newPost = mPostDatabase.push();
+
+
+                    Map<String, String> dataToSave = new HashMap<>();
+                    dataToSave.put("title", titleVal);
+                    dataToSave.put("desc", descVal);
+                    dataToSave.put("image", downloadUrl.toString());
+                    dataToSave.put("timestamp", String.valueOf(java.lang.System.currentTimeMillis()));
+                    dataToSave.put("userid", mUser.getUid());
+                    dataToSave.put("username", mUser.getEmail());
+
+                    newPost.setValue(dataToSave);
+                    mProgress.dismiss();
+
+                    startActivity(new Intent(AddPostActivity.this, PostListActivity.class));
+                    finish();
+
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/example/blogapp/Activities/PostListActivity.java
+++ app/src/main/java/com/example/blogapp/Activities/PostListActivity.java
@@ -15,14 +15,21 @@ import com.example.blogapp.Model.Blog;
 import com.example.blogapp.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PostListActivity extends AppCompatActivity {
     private DatabaseReference mDatabaseReference;
+    private RecyclerView recyclerView;
+    private BlogRecyclerAdapter blogRecyclerAdapter;
+    private List<Blog> blogList;
     private FirebaseDatabase mDatabase;
     private FirebaseUser mUser;
     private FirebaseAuth mAuth;
@@ -38,6 +45,14 @@ public class PostListActivity extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance();
         mDatabaseReference = mDatabase.getReference().child("MBlog");
         mDatabaseReference.keepSynced(true);
+
+
+        blogList = new ArrayList<>();
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
@@ -52,17 +67,21 @@ public class PostListActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_add:
                 if (mUser != null && mAuth != null) {
+
                     startActivity(new Intent(PostListActivity.this, AddPostActivity.class));
                     finish();
-                }
-                break;
 
+                }
+
+                break;
             case R.id.action_signout:
+
                 if (mUser != null && mAuth != null) {
                     mAuth.signOut();
 
                     startActivity(new Intent(PostListActivity.this, MainActivity.class));
                     finish();
+
                 }
                 break;
 
@@ -71,6 +90,46 @@ public class PostListActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mDatabaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                Blog blog = dataSnapshot.getValue(Blog.class);
+
+                blogList.add(blog);
+
+                Collections.reverse(blogList);
+
+                blogRecyclerAdapter = new BlogRecyclerAdapter(PostListActivity.this,blogList);
+                recyclerView.setAdapter(blogRecyclerAdapter);
+                blogRecyclerAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
 
+    }
 }
